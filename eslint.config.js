9fce7ab0d7c78868d5ea11ca62fc8f0ddea60320
Marcos Patricio <mampatricio@hotmail.com>
2024-11-28 12:18:10 -0300

    fix: atalização lib's

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import jsConfig from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  // Configuração para arquivos JS
+  {
+    files: ["**/*.js"],
+    ...jsConfig.configs.recommended,
+  },
+
+  // Configuração para arquivos TS (ESM)
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    ignores: ["dist/**", "node_modules/**"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.esm.json", // Ajuste aqui para o arquivo correto
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules, // Regras recomendadas do TypeScript
+    },
+  },
+
+  // Configuração Prettier (evita conflitos)
+  {
+    files: ["**/*.{js,ts,tsx}"],
+    rules: {
+      ...prettierConfig.rules,
+    },
+  },
+];
+
