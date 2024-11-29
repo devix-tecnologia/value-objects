a91a097b6d38494bf70329fcfa14a6a0f29238a4
Sidarta Veloso <sidartaveloso@gmail.com>
2024-11-29 09:59:56 -0300

    chore: Altera do tipo any para unknown

--- src/types/base_class.ts
+++ src/types/base_class.ts
@@ -2,13 +2,13 @@ interface IBaseClassNumbers {
   get formatted(): string
   get onlyNumbers(): string
   isValid(config: { raiseException: boolean }): boolean
-  equals(other: any): boolean
+  equals(other: unknown): boolean
 }
 
 interface IBaseClassText {
   get formatted(): string
   isValid(config: { raiseException: boolean }): boolean
-  equals(other: any): boolean
+  equals(other: unknown): boolean
 }
 
 export type { IBaseClassNumbers, IBaseClassText }
